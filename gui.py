2a25edf5cc3e8e8549cd7793ffe14c4517d84a6a
Jaju <Madhu.Jaju@hughes.com>
2025-12-19 09:21:48 -0500

    todo implementation

--- gui.py
+++ gui.py
@@ -4,8 +4,12 @@ import FreeSimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip = "Enter todo", key="todo")
 add_button = sg.Button("Add")
+listbox = sg.Listbox(values=functions.get_todos(), key="todos",
+                     enable_events=True, size=(50, 10))
+edit_button = sg.Button("Edit")
 
-window = sg.Window("To-Do List", [[label], [input_box, add_button]],
+window = sg.Window("To-Do List", [[label], [input_box, add_button],
+                                  [listbox, edit_button]],
                    font =('Helvetica', 20))
 
 while True:
@@ -18,6 +22,17 @@ while True:
             new_todo = values['todo']+ "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values = todos)
+        case "todos":
+            window['todo'].update(value = values['todos'][0])
         case sg.WIN_CLOSED:
             break
 
